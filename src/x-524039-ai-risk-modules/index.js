d8d33c78b9ce5e281658d66e69c9781aed0c42ba
Brandon Raphael <brandonraphael1@gmail.com>
2020-09-19 02:20:08 -0700

    added model view for risks

--- src/x-524039-ai-risk-modules/index.js
+++ src/x-524039-ai-risk-modules/index.js
@@ -2,60 +2,33 @@ import {createCustomElement, actionTypes} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
 import { createHttpEffect } from "@servicenow/ui-effect-http";
+import "@servicenow/now-modal";
+import "@servicenow/now-button";
 import "@servicenow/now-loader";
-// Http Effect Function
-// This will be executed when triggered by some action
-// async function httpEffect(url, options, coeffects) {
-// 	const {action, dispatch} = coeffects;
-//
-// 	dispatch('FETCH_STARTED');
-// 	try {
-// 		const result = await fetch(url, options);
-// 		dispatch('FETCH_SUCCEEDED', result);
-// 	} catch(e) {
-// 		dispatch('FETCH_FAILED', e, {} /* meta */, true /* error */);
-// 	}
-// }
 
-// Create the effect
-// Function that takes in some arguments and creates an effect
-// This is generic and can be reused anywhere http requests are needed
-// function createHttpEffect(url, options) {
-// 	return {
-// 		effect: httpEffect,
-// 		args: [url, options]
-// 	};
-// }
-
-// Create the effect for fetching a user
-const fetchUserEffect = ({ properties, dispatch }) => {
+const fetchFoundationalRisksEffect = ({ properties, dispatch }) => {
 	dispatch("GET_FOUNDATIONAL_RISKS", {
 		table: "x_524039_pericrora_ai_foundational_risk",
 		// sysparm_query: `short_descriptionLIKE${properties.searchText}`,
 	});
 };
-// Handle when user fetch succeeded: log the result
-const handleFetchUserSucceeded = ({action, updateState}) => {
-	console.log(action.payload);
-	action.payload.result.map((result) => (
-		console.log(result)
-	))
+
+const handleFetchSucceeded = ({action, updateState}) => {
+	console.log("Fetch succeeded!");
 }
 
-// Handle when user fetch failed: alert failure message
-const handleFetchUserFailed = ({action}) => {
-	console.log("User fetch failed!");
+const handleFetchFailed = ({action}) => {
+	console.log("Fetch failed!");
 }
 
-const handleFetchStarted = ({action}) => console.log("Started fetch of table");
+const handleFetchStarted = ({action}) => {
+	console.log("Started fetch of table...");
+}
 
-const view = (state, { dispatch }) => {
-	// function buttonClicked() {
-	// 	dispatch('USER_FETCHED');
-	// }
+const view = (state, { dispatch, updateState }) => {
 	let foundationalRisks;
 
-	if (state.showLoading) {
+	if (state.showFoundationalRisksLoading) {
 		foundationalRisks = <now-loader id="foundational_risks_loader"></now-loader>
 	} else {
 		foundationalRisks =
@@ -68,25 +41,48 @@ const view = (state, { dispatch }) => {
 				<ul id="foundational_risks">
 					{state.foundationalRisks.length ? (
 						state.foundationalRisks.map((result) => (
-							<li>
+							<li id={"list_item"}>
 								{result.name}
-								<a href={"https://www.google.com"} id="view_foundational_risk">View</a>
+								<now-button id="view_foundational_risk" on-click={() => updateState({ selectedResult: result })}>View</now-button>
 							</li>
 						))
 					) : (
 						<li>No Foundational Risks Found</li>
 					)}
+					<now-button id="create_new_foundational_risk" >Create New FR</now-button>
+					{state.selectedResult ? (
+						<now-modal
+							opened={state.selectedResult}
+							size="lg"
+							footerActions={[
+								{
+									label: "Done",
+									variant: "secondary",
+									clickActionType: "NOW_MODAL#OPENED_SET",
+								},
+							]}
+						>
+							<h1>{state.selectedResult.name}</h1>
+							Foundational Risk
+							<br/>
+							Date Created: {state.selectedResult.sys_created_on}
+							<br/>
+							Created By: {state.selectedResult.sys_created_by}
+							<br/>
+							Description: {state.selectedResult.description}
+						</now-modal>
+					) : null}
 				</ul>
 			</div>
 	}
 
 	return (
 		<div>
-			<h1>
+			<h1 id={"admin_control_panel_title"}>
 				Admin Control Panel
-				<button id="create_ticket">
+				<now-button id="create_ticket">
 					Create Ticket
-				</button>
+				</now-button>
 			</h1>
 			<div id="admin_control_panel">
 				{foundationalRisks}
@@ -100,42 +96,23 @@ createCustomElement('x-524039-ai-risk-modules', {
 	renderer: {type: snabbdom},
 	view,
 	actionHandlers: {
-		[actionTypes.COMPONENT_CONNECTED]: fetchUserEffect,
-		// This would be dispatched within the component's view or COMPONENT_CONNECTED action handler
-		'USER_FETCHED': fetchUserEffect,
-		// This is dispatched from the effect on success, handle it here
-		'FETCH_SUCCEEDED': handleFetchUserSucceeded,
+		[actionTypes.COMPONENT_CONNECTED]: fetchFoundationalRisksEffect,
 		GET_FOUNDATIONAL_RISKS: createHttpEffect("/api/now/table/:table", {
 			pathParams: ["table"],
 			startActionType: "GET_FOUNDATIONAL_RISKS_STARTED",
 			successActionType: "GET_FOUNDATIONAL_RISKS_FETCHED",
 		}),
 		GET_FOUNDATIONAL_RISKS_STARTED: ({ updateState }) =>
-			updateState({ showLoading: true }),
+			updateState({ showFoundationalRisksLoading: true }),
 		GET_FOUNDATIONAL_RISKS_FETCHED: ({ action, updateState }) => {
 			action.payload.result.map((result) => (
 				console.log(result)
 			))
 
-			updateState({ foundationalRisks: action.payload.result, showLoading: false });
+			updateState({ foundationalRisks: action.payload.result, showFoundationalRisksLoading: false });
 		},
-		// 'FETCH_SUCCEEDED': (coeffects) => {
-
-			// const { action, updateState } = coeffects;
-			// const {
-			// 	action: {
-			// 		payload
-			// 	}
-			// } = coeffects;
-			// console.log(payload.result);
-
-			// const { result } = action.payload;
-
-			// updateState({ result });
-
-		// },
-		// This is dispatched if the fetch failed, handle it here
-		'FETCH_FAILED': handleFetchUserFailed,
+		'FETCH_SUCCEEDED': handleFetchSucceeded,
+		'FETCH_FAILED': handleFetchFailed,
 		'FETCH_STARTED': handleFetchStarted,
 	},
 	styles

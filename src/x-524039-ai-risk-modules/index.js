8a44d98a42cc2a075ffb86ab61c012c37757e6b3
Brandon Raphael <brandonraphael1@gmail.com>
2020-09-19 15:07:13 -0700

    added master issues and business units

--- src/x-524039-ai-risk-modules/index.js
+++ src/x-524039-ai-risk-modules/index.js
@@ -6,10 +6,20 @@ import "@servicenow/now-modal";
 import "@servicenow/now-button";
 import "@servicenow/now-loader";
 
-const fetchFoundationalRisksEffect = ({ properties, dispatch }) => {
+const fetchBusinessUnitsEffect = ({}) => {
+	console.log("yoyoyoyo");
+}
+
+const fetchTablesEffect = ({ properties, dispatch }) => {
 	dispatch("GET_FOUNDATIONAL_RISKS", {
 		table: "x_524039_pericrora_ai_foundational_risk",
 		// sysparm_query: `short_descriptionLIKE${properties.searchText}`,
+	});
+	dispatch("GET_BUSINESS_UNITS", {
+		table: "x_524039_pericrora_ai_business_unit",
+	});
+	dispatch("GET_MASTER_ISSUES", {
+		table: "x_524039_pericrora_ai_master_issue",
 	});
 };
 
@@ -27,29 +37,28 @@ const handleFetchStarted = ({action}) => {
 
 const view = (state, { dispatch, updateState }) => {
 	let foundationalRisks;
-
 	if (state.showFoundationalRisksLoading) {
-		foundationalRisks = <now-loader id="foundational_risks_loader"></now-loader>
+		foundationalRisks = <now-loader id="loader"></now-loader>
 	} else {
 		foundationalRisks =
 			<div>
-				<h2 id="foundational_risks_title">
+				<h2 id="title">
 					<u>
 						Foundational Risks
 					</u>
 				</h2>
-				<ul id="foundational_risks">
+				<ul id="list">
 					{state.foundationalRisks.length ? (
 						state.foundationalRisks.map((result) => (
 							<li id={"list_item"}>
 								{result.name}
-								<now-button id="view_foundational_risk" on-click={() => updateState({ selectedResult: result })}>View</now-button>
+								<now-button id="view" on-click={() => updateState({ selectedResult: result })}>View</now-button>
 							</li>
 						))
 					) : (
 						<li>No Foundational Risks Found</li>
 					)}
-					<now-button id="create_new_foundational_risk" >Create New FR</now-button>
+					<now-button id="create_new" >Create New FR</now-button>
 					{state.selectedResult ? (
 						<now-modal
 							opened={state.selectedResult}
@@ -64,12 +73,114 @@ const view = (state, { dispatch, updateState }) => {
 						>
 							<h1>{state.selectedResult.name}</h1>
 							Foundational Risk
-							<br/>
+							<br/><br/>
 							Date Created: {state.selectedResult.sys_created_on}
-							<br/>
+							<br/><br/>
 							Created By: {state.selectedResult.sys_created_by}
-							<br/>
+							<br/><br/>
 							Description: {state.selectedResult.description}
+						</now-modal>
+					) : null}
+				</ul>
+			</div>
+	}
+
+	let businessUnits;
+	if (state.showBusinessUnitsLoading) {
+		businessUnits = <now-loader id="loader"></now-loader>
+	} else {
+		businessUnits =
+			<div>
+				<h2 id="title">
+					<u>
+						Business Units
+					</u>
+				</h2>
+				<ul id="list">
+					{state.businessUnits.length ? (
+						state.businessUnits.map((result) => (
+							<li id={"list_item"}>
+								{result.name}
+								<now-button id="view" on-click={() => updateState({ selectedBusinessUnit: result })}>View</now-button>
+							</li>
+						))
+					) : (
+						<li>No Business Units Found</li>
+					)}
+					<now-button id="create_new" >Create New BU</now-button>
+					{state.selectedBusinessUnit ? (
+						<now-modal
+							opened={state.selectedBusinessUnit}
+							size="lg"
+							footerActions={[
+								{
+									label: "Done",
+									variant: "secondary",
+									clickActionType: "NOW_MODAL#OPENED_SET",
+								},
+							]}
+
+						>
+							<h1>{state.selectedBusinessUnit.name}</h1>
+							Business Unit
+							<br/><br/>
+							Date Created: {state.selectedBusinessUnit.sys_created_on}
+							<br/><br/>
+							Created By: {state.selectedBusinessUnit.sys_created_by}
+							<br/><br/>
+							Goal: {state.selectedBusinessUnit.goal}
+							<br/><br/>
+							Description: {state.selectedBusinessUnit.description}
+						</now-modal>
+					) : null}
+				</ul>
+			</div>
+	}
+
+	let masterIssues;
+	if (state.showMasterIssuesLoading) {
+		masterIssues = <now-loader id="loader"></now-loader>
+	} else {
+		masterIssues =
+			<div>
+				<h2 id="title">
+					<u>
+						Master Issues
+					</u>
+				</h2>
+				<ul id="list">
+					{state.masterIssues.length ? (
+						state.masterIssues.map((result) => (
+							<li id={"list_item"}>
+								{result.name}
+								<now-button id="view" on-click={() => updateState({ selectedMasterIssue: result })}>View</now-button>
+							</li>
+						))
+					) : (
+						<li>No Master Issues Found</li>
+					)}
+					<now-button id="create_new" >Create New MI</now-button>
+					{state.selectedMasterIssue ? (
+						<now-modal
+							opened={state.selectedMasterIssue}
+							size="lg"
+							footerActions={[
+								{
+									label: "Done",
+									variant: "secondary",
+									clickActionType: "NOW_MODAL#OPENED_SET",
+								},
+							]}
+
+						>
+							<h1>{state.selectedMasterIssue.name}</h1>
+							Master Issue
+							<br/><br/>
+							Date Created: {state.selectedMasterIssue.sys_created_on}
+							<br/><br/>
+							Created By: {state.selectedMasterIssue.sys_created_by}
+							<br/><br/>
+							Description: {state.selectedMasterIssue.description}
 						</now-modal>
 					) : null}
 				</ul>
@@ -86,6 +197,10 @@ const view = (state, { dispatch, updateState }) => {
 			</h1>
 			<div id="admin_control_panel">
 				{foundationalRisks}
+				<br/>
+				{businessUnits}
+				<br/>
+				{masterIssues}
 			</div>
 		</div>
 	);
@@ -96,7 +211,7 @@ createCustomElement('x-524039-ai-risk-modules', {
 	renderer: {type: snabbdom},
 	view,
 	actionHandlers: {
-		[actionTypes.COMPONENT_CONNECTED]: fetchFoundationalRisksEffect,
+		[actionTypes.COMPONENT_CONNECTED]: fetchTablesEffect,
 		GET_FOUNDATIONAL_RISKS: createHttpEffect("/api/now/table/:table", {
 			pathParams: ["table"],
 			startActionType: "GET_FOUNDATIONAL_RISKS_STARTED",
@@ -108,8 +223,33 @@ createCustomElement('x-524039-ai-risk-modules', {
 			action.payload.result.map((result) => (
 				console.log(result)
 			))
-
 			updateState({ foundationalRisks: action.payload.result, showFoundationalRisksLoading: false });
+		},
+		GET_BUSINESS_UNITS: createHttpEffect("/api/now/table/:table", {
+			pathParams: ["table"],
+			startActionType: "GET_BUSINESS_UNITS_STARTED",
+			successActionType: "GET_BUSINESS_UNITS_FETCHED",
+		}),
+		GET_BUSINESS_UNITS_STARTED: ({ updateState }) =>
+			updateState({ showBusinessUnitsLoading: true }),
+		GET_BUSINESS_UNITS_FETCHED: ({ action, updateState }) => {
+			action.payload.result.map((result) => (
+				console.log(result)
+			))
+			updateState({ businessUnits: action.payload.result, showBusinessUnitsLoading: false });
+		},
+		GET_MASTER_ISSUES: createHttpEffect("/api/now/table/:table", {
+			pathParams: ["table"],
+			startActionType: "GET_MASTER_ISSUES_STARTED",
+			successActionType: "GET_MASTER_ISSUES_FETCHED",
+		}),
+		GET_MASTER_ISSUES_STARTED: ({ updateState }) =>
+			updateState({ showMasterIssuesLoading: true }),
+		GET_MASTER_ISSUES_FETCHED: ({ action, updateState }) => {
+			action.payload.result.map((result) => (
+				console.log(result)
+			))
+			updateState({ masterIssues: action.payload.result, showMasterIssuesLoading: false });
 		},
 		'FETCH_SUCCEEDED': handleFetchSucceeded,
 		'FETCH_FAILED': handleFetchFailed,
